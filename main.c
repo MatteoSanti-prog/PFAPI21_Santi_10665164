ad13f27198be94b3253787ee36401b0342c4f8a9
MatteoSanti-prog <10665164@polimi.it>
2021-08-15 15:28:37 +0200

    [update] first correct version

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #define INF -1
+#define CONST 3000
 //structure of a graph
 typedef struct {
     unsigned int order;//it represents the number of 'arrival' of the graph
-    unsigned long long int path;//it represents the sum of all minimum spanning trees
+    unsigned int path;//it represents the sum of all minimum spanning trees
     struct Matrix *next;//pointer to the next element of the list (it's ordered from minimum path to the maximum one)
 } Matrix;
 
@@ -15,25 +16,55 @@ typedef Matrix *Pointer;
 //structure of a node
 typedef struct{
     unsigned int node;//it represents the index of the node
-    unsigned long long int distance;//it represents the current distance of the node from node 0
+    unsigned int distance;//it represents the current distance of the node from node 0
     struct Node *next;//pointer to the next element of the list
 }Node;
 //pointer to an element of the nodeList
 typedef Node *PointerNode;
 
-//this function initializes matrix: its parameters are the returned matrix and its dimension
-void initializeMatrix(unsigned int matrix[], unsigned int d){
-    int count1, count2;//counters used in cycle for
-    for(count1=0;count1<d;count1++){
-        for(count2=0;count2<d-1;count2++){
-            if(scanf("%d,", &matrix[count2+(count1*d)])==EOF){
-                exit(1);
+unsigned int parseInt(unsigned int matrix[], const char string[], unsigned int count1, unsigned int d){
+    unsigned int allZero=0;
+    char temp[10];
+    unsigned int i=0;
+    unsigned int counter;
+    unsigned int element=0;
+    for(counter=0;string[counter]!='\0';counter++){
+        if(string[counter]==',' || string[counter]=='\n'){
+            temp[i]='\0';
+            matrix[(count1*d)+element]=atoi(temp);
+            if(count1==0 && element!=0 && matrix[(count1*d)+element]==0){
+                allZero++;
             }
+            element++;
+            i=0;
+            continue;
         }
-        if(scanf("%d", &matrix[count2+(count1*d)])==EOF){
-            exit(1);
+        else{
+            temp[i]=string[counter];
+            i++;
         }
     }
+    return allZero;
+}
+
+//this function initializes matrix: its parameters are the returned matrix and its dimension
+int initializeMatrix(unsigned int matrix[], unsigned int d){
+    unsigned int count1;
+    int temp=1;
+    char string[CONST];
+    for(count1=0;count1<d;count1++){
+        if(fgets(string,CONST,stdin)==NULL){
+            exit(1);
+        }
+        if(string[0]=='\n'){
+            count1--;
+            continue;
+        }
+        if(parseInt(matrix, string, count1, d)==d && count1==0){
+            temp=0;
+        }
+    }
+    return temp;
 }
 
 
@@ -62,7 +93,7 @@ void insertNode(PointerNode *listOfNodes, unsigned int i){
 }
 
 //this function inserts a new graph in base of its minimum path's length
-void insertInOrder(Pointer *ranking, unsigned int order, unsigned long long int path, unsigned int k){
+void insertInOrder(Pointer *ranking, unsigned int order, unsigned int path, unsigned int k){
     unsigned int counter=0;
     Matrix *punt, *currentPunt, *previousPunt;
     previousPunt=NULL;
@@ -127,7 +158,7 @@ void deleteNode(PointerNode *listOfNodes, unsigned int node){
     }
 }
 
-void updateDistance(PointerNode *listOfNodes, unsigned int node, unsigned long long int newDistance){
+void updateDistance(PointerNode *listOfNodes, unsigned int node, unsigned int newDistance){
     Node *punt, *currentPunt, *prevPunt;
     prevPunt=NULL;
     currentPunt=*listOfNodes;
@@ -148,14 +179,10 @@ void updateDistance(PointerNode *listOfNodes, unsigned int node, unsigned long l
     deleteNode((PointerNode *) &(punt->next), node);
 }
 
-unsigned int getNode(PointerNode *listOfNodes){
-    return (*listOfNodes)->node;
-}
-
 //this function returns the sum of all minimum paths, and it implements Dijkstra's algorithm
-unsigned long long int graphCost(unsigned const int matrix[], unsigned int d, PointerNode listOfNodes, unsigned long long int distances[]){
+unsigned long long int graphCost(unsigned const int matrix[], unsigned int d, PointerNode listOfNodes, unsigned int distances[]){
     unsigned int j;//generic node
-    unsigned long long int sum=0;
+    unsigned int sum=0;
     unsigned int chosen;//node with minimum distance
     for(j=0;j<d;j++){
         insertNode(&listOfNodes,d-j-1);
@@ -167,7 +194,7 @@ unsigned long long int graphCost(unsigned const int matrix[], unsigned int d, Po
         }
     }
     while(emptyList(&listOfNodes)==0){
-        chosen=getNode(&listOfNodes);//node with minimum distance from node 0
+        chosen=listOfNodes->node;//node with minimum distance from node 0
         deleteElement(&listOfNodes);
         if(distances[chosen]==INF){
             sum+=0;
@@ -229,18 +256,22 @@ int main() {
     //unsigned int* matrix;//use malloc in order to initialize the matrix because i don't know its dimension at compile time
     Pointer ranking;//ranking ordered by minimum path
     PointerNode listOfNodes;
-    if(scanf("%d %d", &d, &k)==EOF){
+    if(scanf("%i %i", &d, &k)==EOF){
         exit(1);
     }
     unsigned int matrix[d*d];
-    unsigned long long int distances[d];
+    unsigned int distances[d];
     initializeRanking(&ranking);
     initializeNode(&listOfNodes);
     while(scanf("%s", command)!=EOF){
         if(strcmp(command,"AggiungiGrafo")==0){
             if(d!=0) {
-                initializeMatrix(matrix, d);//initialization of the matrix
-                insertInOrder(&ranking, counter, graphCost(matrix, d, listOfNodes, distances), k);
+                if(initializeMatrix(matrix, d)==0){//initialization of the matrix
+                    insertInOrder(&ranking, counter, 0, k);
+                }
+                else{
+                    insertInOrder(&ranking, counter, graphCost(matrix, d, listOfNodes, distances), k);
+                }
                 counter++;
             }
         }
